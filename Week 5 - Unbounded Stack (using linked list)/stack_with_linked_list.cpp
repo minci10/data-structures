475334cdd70368398014ff581c684b7bc4ef05a5
Muhammet İnci <59832541+MuhammetInci@users.noreply.github.com>
2020-04-08 03:42:51 +0200

    Add files via upload

---
+++ Week 5 - Unbounded Stack (using linked list)/stack_with_linked_list.cpp
@@ -0,0 +1,58 @@
+// stack_with_linked_list.cpp : Defines the entry point for the console application.
+//
+/*
+The program should use all these methods to make the operations as follows:
+1) create a stack containing the numbers given by the user,
+2) print the contents of the stack,
+3) remove two values from the stack and print them,
+4) print the top number,
+5) print the number of values on the stack if it is nonempty, or an appropriate message
+otherwise,
+6) clear the stack.
+*/
+
+
+#include "stack_with_linked_list.h"
+
+
+int main()
+{
+
+	Stack s1;
+
+	s1.push(1);
+	s1.push(2);
+	s1.push(3);
+	s1.push(4);
+	s1.push(5);
+
+	s1.print(); //5 4 3 2 1
+	std::cout << "Size: " << s1.size() << std::endl; //5
+	std::cout << "Pop: " << s1.pop() << std::endl; //5
+	std::cout << "Size: " << s1.size() << std::endl; //4
+	std::cout << "Top: " << s1.top() << std::endl; //4
+	s1.print(); //4 3 2 1
+
+	Stack s2 = s1;
+
+	s2.print(); //4 3 2 1
+	s2.push(6);
+	s2.print(); //6 4 3 2 1
+
+	s1 = s2;
+
+	if (s1 == s2)
+		std::cout << "The stacks are equal." << std::endl; //yes
+
+	std::cout << "Pop: " << s1.pop() << std::endl; //6
+	s1.print(); //4 3 2 1
+	s2.print(); //6 4 3 2 1
+
+	s2.clear();
+	s1.print(); //4 3 2 1
+	s2.print(); //empty
+
+	
+    return 0;
+}
+
---
+++ Week 5 - Unbounded Stack (using linked list)/stack_with_linked_list.h
@@ -0,0 +1,221 @@
+/*
+Write a program which implements an unbounded stack (a Last-In-First-Out structure using
+non-sequential memory storage – the linked list), aimed at storing real numbers, and a set of
+methods operating on this structure:
+• the default counstructor,
+• the copy-constructor (with memory allocation),
+• the assignment operator= (with memory dellocation and allocation),
+• the comparison operator==,
+• the destructor (with memory deallocation),
+• push – adding a real number to the stack (with memory allocation),
+• pop – removig from the stack a real number pushed there most recently or throwing
+an empty stack exception (with memory deallocation),
+• top – returning the most recently pushed number or throwing an empty stack
+exception,
+• print – printing the values which are on the stack currently, starting from the one
+pushed recently,
+• empty – informing whether the stack is empty,
+• size – returning the number of the items on the stack,
+• clear – removing all the items from the stack (with memory deallocation).
+*/
+
+#include <iostream>
+#include <stdexcept>
+
+struct Stack {
+
+private:
+
+	struct Node{
+
+		int value;
+		Node* next;
+
+		Node(int v, Node* n = nullptr) {
+			value = v;
+			next = n;
+		}
+
+		//~Node() { std::cout << value << " destroyed" << std::endl; }
+
+	};
+	Node* head = nullptr;
+	int counter = 0;
+
+public:
+
+	Stack() = default;	
+	Stack(const Stack&);
+	~Stack();
+	void push(int);
+	int pop();
+	void clear();
+	int size() const;
+	bool empty() const;
+	void print() const;
+	int top() const;
+	Stack operator=(const Stack& s);
+	bool operator==(const Stack& s)const;
+
+};
+
+
+
+Stack::Stack(const Stack& s) {
+
+	Node* walker = s.head;
+	for (int i = s.counter-1; i >= 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			walker = walker->next;
+		}
+		Node* a = new Node(walker->value, head);
+		head = a;
+		walker = s.head;
+	}
+
+	counter = s.counter;
+
+}
+
+Stack::~Stack() {
+
+	while (head != nullptr)
+	{
+		Node* killer = head;
+		head = killer->next;
+		delete killer; 
+		
+	}
+
+}
+
+void Stack::push(int value) {
+
+	Node* creator = new Node(value,head);
+	head = creator;
+	counter++;
+
+}
+
+void Stack::print()const {
+
+	if (counter == 0)
+	{
+		std::cout << "stack is empty." << std::endl;
+	}
+	else {
+
+		Node* walker = head;
+		while(walker != nullptr)
+		{
+			std::cout << walker->value << " ";	
+			walker = walker->next;
+		}
+
+		std::cout << std::endl;
+
+	
+	}
+	
+}
+
+int Stack::pop() {
+
+	if (counter == 0)
+	{
+		throw std::logic_error("stack is empty.");
+	}
+
+	Node* killer = head;
+	int value = killer->value;
+	head = killer->next;
+	delete killer;
+	counter--;
+	return value;
+
+}
+
+int Stack::size()const {
+
+	return counter;
+
+}
+
+int Stack::top()const {
+
+	if (counter==0)
+	{
+		throw std::logic_error("stack is empty.");
+	}
+	return head->value;
+
+}
+
+bool Stack::empty()const {
+
+	if (counter==0)
+	{
+		return true;
+	}
+	else return false;
+
+}
+
+void Stack::clear() {
+
+	while (head != nullptr)
+	{
+		Node* killer = head;
+		head = killer->next;
+		delete killer;
+
+	}
+	counter = 0;
+}
+
+bool Stack::operator==(const Stack& s)const {
+
+	if (counter == s.counter)
+	{
+		Node* walker = head;
+		Node* walker2 = s.head;
+		while (walker != nullptr)
+		{
+			if (walker->value != walker2->value)
+			{
+				return false;
+			}
+			walker = walker->next;
+			walker2 = walker2->next;
+		}
+		return true;
+	}
+	else return false;
+}
+
+Stack Stack::operator=(const Stack& s) {
+
+	while (head != nullptr)
+	{
+		Node* killer = head;
+		head = killer->next;
+		delete killer;
+	}
+	head = nullptr;
+	Node* walker = s.head;
+	for (int i = s.counter - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			walker = walker->next;
+		}
+		Node* a = new Node(walker->value, head);
+		head = a;
+		walker = s.head;
+	}
+	counter = s.counter;
+	return *this;
+}
+
