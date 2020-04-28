d0c586f7d537aede34af118646580f4cfd3ad4c0
Muhammet İnci <59832541+MuhammetInci@users.noreply.github.com>
2020-04-28 17:34:31 +0200

    Add files via upload

---
+++ Week 7 - Unbounded Vector Container (as a template structure)/unbounded_vector.h
@@ -0,0 +1,205 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+template<typename T>
+struct Vector {
+
+private:
+	T* data = nullptr;
+	int capacity = 1;
+	int counter = 0;
+
+public:
+	Vector();
+
+	~Vector();
+
+	Vector(const Vector&);
+
+	void push_back(T);
+
+	void pop_back();
+
+	T front() const;
+
+	T back() const;
+
+	bool empty() const;
+
+	int size() const;
+
+	void clear();
+
+	Vector& operator=(const Vector&);
+
+	bool operator==(const Vector&) const;
+
+	T& operator[](int) const;
+
+
+};
+
+template<typename T>
+Vector<T>::Vector() {
+
+	data = new T[capacity];
+
+}
+
+template<typename T>
+Vector<T>::~Vector() {
+
+	delete[] data;
+
+}
+
+template<typename T>
+void Vector<T>::push_back(T value) {
+
+	if (counter==capacity)
+	{
+		capacity *= 2;
+		T* tmp = new T[capacity];
+		for (int i = 0; i < counter; i++)
+		{
+			tmp[i] = data[i];
+		}
+		delete[] data;
+		data = tmp;
+	}
+
+	data[counter++] = value;
+
+}
+
+template<typename T>
+void Vector<T>::pop_back() {
+
+	if (counter == 0)
+	{
+		throw logic_error("Vector is empty");
+	}
+	counter--;
+
+}
+
+template<typename T>
+T Vector<T>::front() const {
+
+	if (counter == 0)
+	{
+		throw logic_error("Vector is empty");
+	}
+	return data[0];
+
+}
+
+template<typename T>
+T Vector<T>::back() const {
+
+	if (counter == 0)
+	{
+		throw logic_error("Vector is empty");
+	}
+	return data[counter-1];
+
+}
+
+template<typename T>
+int Vector<T>::size() const {
+
+	return counter;
+	
+}
+
+template<typename T>
+T& Vector<T>::operator[](int index) const {
+
+	if (index < 0 || index >= counter)
+	{
+		throw out_of_range("Out of vector range");
+	}
+
+	return data[index];
+
+}
+
+template<typename T>
+bool Vector<T>::empty()const {
+
+	if (counter > 0)
+	{
+		return true;
+	}
+
+	else return false;
+
+}
+
+template<typename T>
+bool Vector<T>::operator==(const Vector<T>& v)const {
+
+	bool eq = true;
+	if (counter != v.counter)
+	{
+		eq = false;
+	}
+	else {
+
+		for (int i = 0; i < counter; i++)
+		{
+			if (data[i] != v.data[i])
+			{
+				eq = false;
+			}
+		}
+
+	}
+	return eq;
+}
+
+
+
+template<typename T>
+Vector<T>::Vector(const Vector<T>& v) {
+
+
+	data = new T[capacity];
+
+	for (int i = 0; i < v.counter; i++)
+	{
+		push_back(v.data[i]);
+	}
+
+
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& v) {
+
+	delete[] data;
+	capacity = 1;
+	counter = 0;
+	data = new T[capacity];
+	for (int i = 0; i < v.counter; i++)
+	{
+		push_back(v.data[i]);
+	}
+
+	return *this;
+
+}
+
+template<typename T>
+void Vector<T>::clear() {
+
+	delete[] data;
+	counter = 0;
+	capacity = 1;
+	data = new T[capacity];
+
+}
---
+++ Week 7 - Unbounded Vector Container (as a template structure)/vector_test.cpp
@@ -0,0 +1,66 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include "unbounded_vector.h"
+//#include <vector>
+
+using namespace std;
+
+template<typename T>
+void print(Vector<T>& v) {
+
+	if (v.size() == 0 )
+	{
+		cout << "empty";
+	}
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+		
+	}
+	cout << endl;
+
+}
+
+int main()
+{
+	
+	Vector<int> v1;
+
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v1.push_back(4);
+	v1.push_back(5);
+
+	print(v1); //1 2 3 4 5
+	cout << "Size: " << v1.size() << endl; //5
+	v1.pop_back();
+	cout << "First: " << v1.front() << endl; //1
+	cout << "Last: " << v1.back() << endl; //4
+	cout << "Size: " << v1.size() << endl; //4
+	print(v1); //1 2 3 4
+
+	Vector<int> v2 = v1;
+
+	print(v2); //1 2 3 4
+	v2.push_back(6);
+	print(v2); //1 2 3 4 6
+
+	v1 = v2;
+
+	if (v1 == v2)
+		cout << "The vectors are equal." << endl; //yes
+
+	v1.pop_back();
+	cout << "Last: " << v1.back() << endl; //4
+	print(v1); //1 2 3 4
+	print(v2); //1 2 3 4 6
+
+	v2.clear();
+	print(v1); //1 2 3 4
+	print(v2); //empty
+	
+    return 0;
+}
+
