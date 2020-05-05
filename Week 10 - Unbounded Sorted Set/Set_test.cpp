19ca9e8e3705f7ac2008260f689fc45228c0232a
Muhammet İnci <59832541+MuhammetInci@users.noreply.github.com>
2020-05-05 22:03:43 +0200

    Add files via upload

---
+++ Week 10 - Unbounded Sorted Set/Set_test.cpp
@@ -0,0 +1,60 @@
+/*
+The program should use all these methods to make the operations as follows:
+1) create a set of integers containing the numbers given by the user,
+2) print the number of items stored in the set,
+3) remove a value from the set,
+4) print the number of items stored in the set,
+5) check if a given value is in the set,
+6) clear the set.
+Compare your implementation of the container with the built-in container set delivered in the <set> library
+*/
+
+#include "stdafx.h"
+#include "unbounded_sorted_set.h"
+//#include <set>
+
+using namespace std;
+
+
+int main()
+{
+	
+    Set<int> s1;
+
+    s1.insert(3);
+    s1.insert(2);
+    s1.insert(3);
+    s1.insert(1);
+    s1.insert(4);
+
+    s1.print(); //1 2 3 4
+    cout << "Size: " << s1.size() << endl; //4
+    cout << s1.erase(7) << endl; //0
+    cout << s1.erase(3) << endl; //1
+    s1.print(); //1 2 4
+    cout << "Size: " << s1.size() << endl; //3
+
+    Set<int> s2 = s1;
+
+    s2.print(); //1 2 4
+    s2.insert(5);
+    s2.print(); //1 2 4 5
+
+    s1 = s2;
+
+    if(s1 == s2)
+       cout << "The sets are equal." << endl; //yes
+	
+    cout << s1.count(7) << endl; //0
+    cout << s1.count(2) << endl; //1
+    cout << s1.erase(2) <<endl; //1
+    s1.print(); //1 4 5
+    s2.print(); //1 2 4 5
+				
+    s2.clear();
+    s1.print(); //1 4 5
+    s2.print(); //empty
+
+    return 0;
+}
+
---
+++ Week 10 - Unbounded Sorted Set/unbounded_sorted_set.h
@@ -0,0 +1,256 @@
+/*
+Write a program which implements an unbounded sorted set (using the single or doublelinked list) as a template structure (aimed at storing unique 
+values of any type and ordering them in ascending order - from the lowest to the greatest), and the methods operating on this structure:
+• the default constructor,
+• the destructor (with memory deallocation),
+• the copy-constructor (with memory allocation),
+• the assignment operator= (with memory dellocation and allocation),
+• the comparison operator==,
+• insert – adding a value to the set in ascending order only if it doesn't exist in the set(with memory allocation),
+• erase – removig the given value from the set if exists (with memory deallocation) and returning the number of items removed (0 or 1),
+• count – returning the number of occurences of a value (0 or 1),
+• empty – informing whether the set is empty,
+• size – returning the number of items in the set,
+• clear – removing all the items from the set (with memory deallocation).
+*/
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+template<typename T>	
+struct Set {
+
+
+private:
+
+	struct Node {
+
+		T value;
+		Node* next;
+
+		Node(T v, Node* n = nullptr) {
+
+			value = v;
+			next = n;
+
+		}
+		
+	};
+
+	Node* head = nullptr;
+	int counter = 0;
+	
+
+public:
+
+
+	Set() = default;
+
+	Set(const Set&);
+
+	~Set();
+
+	void insert(T);
+
+	int erase(T);
+
+	void clear();
+
+	int count(T) const;
+
+	int size() const;
+
+	void print() const;
+
+	bool empty() const;
+
+	Set& operator=(const Set&);
+
+	bool operator==(const Set&) const;
+
+
+};
+
+
+
+template<typename T>
+Set<T>::~Set() {
+
+	clear();
+
+}
+
+template<typename T>
+void Set<T>::print() const {
+
+	if (counter == 0)
+	{
+		cout << "empty";
+	}
+	Node* walker = head;
+	while (walker != nullptr)
+	{
+		cout << walker->value << " ";
+		walker = walker->next;
+	}
+	cout << endl;
+
+}
+
+template<typename T>
+void Set<T>::clear() {
+
+	
+	while (head != nullptr)
+	{
+		Node* killer = head;
+		head = killer->next;
+		delete killer;
+		
+	}
+	counter = 0;
+
+
+}
+
+template<typename T>
+Set<T>::Set(const Set<T> &s) {
+
+	Node* walker = s.head;
+	while (walker != nullptr)
+	{
+		insert(walker->value);
+		walker = walker->next;
+	}
+	
+}
+
+template <typename T>
+Set<T>& Set<T>::operator=(const Set<T> &s) {
+
+	clear();
+	Node* walker = s.head;
+	while (walker != nullptr)
+	{
+		insert(walker->value);
+		walker = walker->next;
+	}
+	return *this;
+
+}
+
+template<typename T>
+int Set<T>::size()const {
+
+	return counter;
+}
+
+template<typename T>
+bool Set<T>::empty()const {
+
+	if (counter == 0)
+	{
+		return true;
+	}
+
+	else return false;
+}
+
+
+template<typename T>
+void Set<T>::insert(T value) {
+
+	Node* pred = nullptr;
+	Node* succ = head;
+	while (succ != nullptr && succ->value < value)
+	{
+		pred = succ;
+		succ = succ->next;
+	}
+	if (succ == nullptr || value < succ->value)
+	{
+		Node* creator = new Node(value,succ);
+		if (pred != nullptr)
+		{
+			pred->next = creator;
+		}
+		else {
+			head = creator;
+		}
+		counter++;
+	}
+
+}
+
+
+template<typename T>
+int Set<T>::erase(T value) {
+
+	Node* pred = nullptr;
+	Node* succ = head;
+	while (succ != nullptr && succ->value < value)
+	{
+		pred = succ;
+		succ = succ->next;
+	}
+	if (succ != nullptr && succ-> value == value)
+	{
+		Node* killer = succ;
+		succ = succ->next;
+		if (pred != nullptr)
+		{
+			pred->next = succ;
+		}
+		else {
+			head = succ;
+		}
+		delete killer;
+		counter--;
+		return 1;
+		
+	}
+	return 0;
+}
+
+template<typename T>
+int Set<T>::count(T value) const {
+
+	
+	Node* walker = head;
+	while (walker != nullptr)
+	{
+		if (walker->value == value)
+		{
+			return 1;
+		}
+		walker = walker->next;
+	}
+	return 0;
+	
+}
+
+template<typename T>
+bool Set<T>::operator==(const Set<T> &s) const {
+
+	if (counter != s.counter)
+	{
+		return false;
+	}
+	else {
+		Node* walker = head;
+		Node* walker2 = s.head;
+		while (walker != nullptr)
+		{
+			if (walker->value != walker2->value)
+			{
+				return false;
+			}
+			walker = walker->next;
+			walker2 = walker2->next;
+		}
+		return true;
+	}
+	
+
+}
